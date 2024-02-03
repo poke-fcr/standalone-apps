0f6dee7975ebd83e4f23cb35b599a881237ad032
vishal <poke.fcr@gmail.com>
2024-02-03 19:07:13 +0530

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'standalone-apps';
+  
+
+  ngOnInit() {
+    const firebaseConfig = {
+      apiKey: 'AIzaSyC1068JtV4Sifq1aFBUuBXYUY9zQK6hke4',
+      authDomain: 'standalone-apps-1998.firebaseapp.com',
+      projectId: 'standalone-apps-1998',
+      storageBucket: 'standalone-apps-1998.appspot.com',
+      messagingSenderId: '259467626106',
+      appId: '1:259467626106:web:ad5b191f30f1a300ccddc3',
+      measurementId: 'G-7KTJ6BDHJF',
+    };
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+  }
 }
